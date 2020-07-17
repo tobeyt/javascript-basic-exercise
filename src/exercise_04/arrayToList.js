dd7a2a09ca7cf55cb06d314662cdd903886e13e4
Tobey <bitsqiu@gmail.com>
2020-07-17 13:19:48 +0800

    fix: seercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,28 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined) {
+    throw new Error("Creating list from undefined array");
+  }
+  if (array === null) {
+    throw new Error("Creating list from undefined array");
+  }
+  if (array.length === 0) {
+    throw new Error("Creating list from empty array");
+  }
+  const res = [];
+  array.forEach((cur) => {
+    const node = {};
+    node.value = cur;
+    node.next = null;
+    res.push(node);
+  });
+  res.forEach((cur, index) => {
+    if (index === res.length - 1) {
+      cur.next = null;
+    } else {
+      cur.next = res[index + 1];
+    }
+  });
+  return res[0]
 }
